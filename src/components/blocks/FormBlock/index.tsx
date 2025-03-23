fa65f7470666f1c5cd5e5c5a72e50d8cd126ebb6
Blitzkr1eg888 <josh.commerces@gmail.com>
2025-03-23 14:43:52 +1100

    Update index.tsx

--- src/components/blocks/FormBlock/index.tsx
+++ src/components/blocks/FormBlock/index.tsx
@@ -5,26 +5,36 @@ import { getComponent } from '../../components-registry';
 import { mapStylesToClassNames as mapStyles } from '../../../utils/map-styles-to-class-names';
 import SubmitButtonFormControl from './SubmitButtonFormControl';
 
+// Global Netlify form shim (for build-time detection)
+function NetlifyFormShim() {
+    return (
+        <form name="demo-call" data-netlify="true" hidden>
+            <input type="text" name="name" />
+            <input type="text" name="Company-name" />
+            <input type="email" name="email" />
+            <input type="text" name="phone" />
+            <select name="company-size">
+                <option>Just me (solo operator / freelancer)</option>
+                <option>2–5 people</option>
+                <option>16–50 people</option>
+                <option>51-100 people</option>
+                <option>101+ people</option>
+            </select>
+        </form>
+
 export default function FormBlock(props) {
     const formRef = React.createRef<HTMLFormElement>();
     const { fields = [], elementId, submitButton, className, styles = {}, 'data-sb-field-path': fieldPath } = props;
 
     if (fields.length === 0) {
-        return (
-             <>
-                {/* Hidden dummy form to allow Netlify to detect this form at build time */}
-                <form name="demo-call" data-netlify="true" hidden>
-                    <input type="text" name="name" />
-                    <input type="email" name="email" />
-                    <textarea name="message"></textarea>
-                </form>
-            </>
-        );
+        return null;
     }
 
 
     return (
-       <form
+       <NetlifyFormShim />
+    
+        <form
     className={classNames(
         'sb-component',
         'sb-component-block',
